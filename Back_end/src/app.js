f35998de9f30a8c158914515e896a06bccc40b5e
shtya <shtya54@gmail.com>
2026-08-09 23:54:41 +0300

    Back_end: core app config (app.js, nodemon, cloudinary config)

--- Back_end/src/app.js
+++ Back_end/src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import routes from './routes/index.js';
 import { errorHandler } from './middlewares/errorHandler.middleware.js';
 
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.json());
+app.use('/fonts', express.static(path.join(__dirname, '../public/fonts'), {
+	setHeaders: res => res.setHeader('Content-Type', 'application/x-protobuf'),
+}));
+app.get('/privacy', (req, res) => res.sendFile(path.join(__dirname, '../public/privacy.html')));
 app.use('/api', routes);
 
 app.use(errorHandler);
